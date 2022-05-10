ac93d6470da72003111a12a0246738d7d0128ac6
Allison Anguiano <aanguiano@dev-10.com>
2022-05-10 10:59:44 -0500

    A bit more added to Web API section

---
+++ EBlockbuster/CustomAttributes/FutureDateAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace EBlockbuster.CustomAttributes
+{
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateTime)
+            {
+                DateTime date = Convert.ToDateTime(value);
+
+                if (date <= DateTime.Today)
+                {
+                    return new ValidationResult("Date must be in the future");
+                }
+                else
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            else
+            {
+                return new ValidationResult("This attribute only works with DateTime objects");
+            }
+
+        }
+    }
+}
---
+++ EBlockbuster/Models/ProductModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EBlockbuster.Models
+{
+    public class ProductModel
+    {
+    }
+}

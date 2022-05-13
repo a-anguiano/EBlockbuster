0d1242f84fa7ac8f761ebb78598dac63dc486d2a
Allison Anguiano <aanguiano@dev-10.com>
2022-05-13 09:27:06 -0500

    Customer GET on Postman

---
+++ EBlockbuster/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using EBlockbuster.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EBlockbuster.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerRepository _customerRepo;
+
+        public CustomerController(ICustomerRepository customerRepo)
+        {
+            _customerRepo = customerRepo;
+        }
+
+        [HttpGet]   
+        [Route("/api/[controller]/{customerId}", Name = "GetCustomer")]
+        public IActionResult GetCustomer(int customerId)
+        {
+            var result = _customerRepo.Get(customerId);
+
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return NotFound(result.Message);
+            }
+        }
+
+
+    }
+}

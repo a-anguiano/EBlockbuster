f1f8f4df78984ea3d33c86e0a4b5b427be2df5a5
Allison Anguiano <aanguiano@dev-10.com>
2022-05-15 21:28:49 -0500

    TopThreeRentedProducts ADO and test complete

--- EBlockbuster.Core/Interfaces/DAL/IReportsRepository.cs
+++ EBlockbuster.Core/Interfaces/DAL/IReportsRepository.cs
@@ -9,11 +9,10 @@ namespace EBlockbuster.Core.Interfaces
 {
     public interface IReportsRepository
     {
-        Response<MostProfitableItem> GetMostProfitable();
-        Response<MostProfitableItemByCategory> GetMostProfitableByCategory(int categoryId);
-        Response<LeastProfitableItem> GetLeastProfitable();
-        Response<LeastProfitableItemByCategory> GetLeastProfitableByCategory(int categoryId);
-        //GetTopRentedMovies    //Top 3?
-        //Response<List<TopThreeProducts>>
+        //Response<MostProfitableItem> GetMostProfitable();
+        //Response<MostProfitableItemByCategory> GetMostProfitableByCategory(int categoryId);
+        //Response<LeastProfitableItem> GetLeastProfitable();
+        //Response<LeastProfitableItemByCategory> GetLeastProfitableByCategory(int categoryId);
+        Response<List<TopThreeRentedProducts>> GetTopThreeRentedProducts();
     }
 }
---
+++ EBlockbuster.DAL.Test/SqlReportsRepositoryTests.cs
@@ -0,0 +1,54 @@
+﻿using EBlockbuster.DAL.ADO;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBlockbuster.DAL.Test
+{
+    public class SqlReportsRepositoryTests
+    {
+        SqlReportsRepository db;
+
+        [SetUp]
+        public void Setup()
+        {
+            ConfigProvider cp = new ConfigProvider();
+            db = new SqlReportsRepository(cp.Config);
+        }
+
+        [Test]
+        public void TestGetTopThreeRentedProducts()
+        {
+            var result = db.GetTopThreeRentedProducts();
+            Assert.AreEqual("Sel8nne", result.Data[0].ProductTitle);
+            Assert.AreEqual("Riding the Bullet", result.Data[1].ProductTitle);
+            Assert.AreEqual("Deadly Mantis, The", result.Data[2].ProductTitle);
+            //could test other columns
+            //or count
+
+        }
+
+        //[Test]
+        //public void TestGetPensionList() // @agencyId - 10, 7
+        //{
+        //    var result = db.GetPensionList(10);
+        //    Assert.AreEqual("Maxine", result.Data[0].AgencyName);
+        //    Assert.AreEqual("Mor Edyth", result.Data[0].NameLastFirst);
+        //    Assert.AreEqual(dateOfBirth, result.Data[0].DateOfBirth);
+        //    Assert.AreEqual(DeactivationDate, result.Data[0].DeactivationDate);
+        //}
+
+        //[Test]
+        //public void TestAuditClearance() // sc = 2   // ag id = 7 
+        //{
+        //    var result = db.AuditClearance(2, 7);
+        //    Assert.AreEqual("Gribble Kirby", result.Data[0].NameLastFirst);
+        //    Assert.AreEqual(dateOfBirth2, result.Data[0].DateOfBirth);
+        //    Assert.AreEqual(ActivationDate, result.Data[0].ActivationDate);
+        //    Assert.AreEqual(DeactivationDate2, result.Data[0].DeactivationDate);
+        //}
+    }
+}

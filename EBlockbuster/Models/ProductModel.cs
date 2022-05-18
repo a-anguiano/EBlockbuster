5e7b8689d3c28f3adc02ed7ac0e97afb850ce678
Allison Anguiano <aanguiano@dev-10.com>
2022-05-18 13:48:35 -0500

    minor changes

--- EBlockbuster/Models/ProductModel.cs
+++ EBlockbuster/Models/ProductModel.cs
@@ -6,13 +6,20 @@ namespace EBlockbuster.Models
     public class ProductModel
     {
         public int ProductId { get; set; }
+
         [Required(ErrorMessage = "Name is required")]
-        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Photo URL is required")]
+        [StringLength(125, ErrorMessage = "Photo URL cannot exceed 125 characters")]
         public string Photo { get; set; }
+
         [Required(ErrorMessage = "Description is required")]
-        [StringLength(250, ErrorMessage = "Description cannot exceed 50 characters")]
+        [StringLength(250, ErrorMessage = "Description cannot exceed 250 characters")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Category ID is required")]
         public int CategoryId { get; set; }
     }
 }
